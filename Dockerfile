9dcf9c16cdab59ade7f20fa90d4cd7628b4092e3
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-16 22:36:45 -0500

    cuarto intento

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:18 as build
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm ci
+
+COPY . .
+
+RUN npm run build
+
+### stage 2
+
+FROM nginx:alpine
+
+ADD ./config/default.conf /etc/nginx/conf.d/default.conf
+
+COPY --from=build /app/dist/adminpro /var/www/app
+
+EXPOSE 3000
+
+CMD ["nginx","-g","daemon off;"]
--- main.tf
+++ main.tf
@@ -4,43 +4,186 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
-}
-
-provider "aws" {
-  region = "us-east-1"  # Cambia la región según tus necesidades
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "gestiondehospitales"
-}
-
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+  required_providers {
+    docker = {
+      source    = "kreuzwerker/docker"
+      version   = "~> 2.20.0"
+    }
+    aws = {
+      source    = "hashicorp/aws"
+      version   = "~> 4.16"
+    }
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+provider "aws" {
+ 
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
+
+variable "imagebuild" {
+  type = string
+  description = "the latest image build version"
+}
+
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "frontGrupo02" # Naming the cluster
+}
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "frontGrupo02" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "frontGrupo02",
+      "image": "ivanzapata2126/frontend:${var.imagebuild}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000,
+          "protocol": "tcp"
+        }
+      ],
+      "memory": 2048,
+      "cpu": 512
+    }
   ]
-
-  bucket = aws_s3_bucket.example.id
-  acl    = "public-read"
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 2048         # Specifying the memory our container requires
+  cpu                      = 512        # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole${var.imagebuild}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-frontGrupo02" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group-frontGrupo02"
+  port        = "3000"
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "3000"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
+
+resource "aws_ecs_service" "my_first_service" {
+  name     =  "frontGrupo02"                           # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = "3000" # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true                                                # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Show the load balancer url (app url)
+output "website_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
+
+#Show the ecs cluster name
+output "cluster_name" {
+  value = aws_ecs_cluster.my_cluster.name
+}
