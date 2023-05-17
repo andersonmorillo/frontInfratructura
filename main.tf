601b7d53a247b700f36d9b5dbf41a14841453ac9
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-16 20:39:05 -0500

    tercer intento

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "fstatefrontend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"  # Cambia la región según tus necesidades
 }
