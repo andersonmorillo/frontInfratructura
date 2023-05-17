1029f353d030adad77416ebbde9b43869fb9ce09
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-17 10:29:29 -0500

    ddd

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_ecs_task_definition" "my_first_task" {
       "portMappings": [
         {
           "containerPort": 3000,
-          "hostPort": 0,
+          "hostPort": 3000,
           "protocol": "tcp"
         }
       ],
