3ea0c665b3854e045c279136f250ca06ea220774
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-16 21:24:50 -0500

    cuarto intento

--- main.tf
+++ main.tf
@@ -6,23 +6,41 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = "us-east-1"  # Cambia la región según tus necesidades
 }
 
-resource "aws_s3_bucket" "static_app_bucket" {
-  bucket = "gestiondehospitales" 
-  acl    = "public-read"
+resource "aws_s3_bucket" "example" {
+  bucket = "gestiondehospitales"
+}
 
-
-  # Configuraciones adicionales del bucket de S3 si es necesario
-  tags = {
-    Name = "Mi aplicación"
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
+
 output "bucket_name" {
-  value = aws_s3_bucket.static_app_bucket.bucket
+  value = aws_s3_bucket.example.bucket
 }
 
