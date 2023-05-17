6d072f0d91eb48eedcefb12b380366325710c687
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-16 20:25:32 -0500

    1er intento aws

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"  # Cambia la región según tus necesidades
+}
+
+resource "aws_s3_bucket" "static_app_bucket" {
+  bucket = "GestiondehospitalesFrontend"  # Cambia el nombre del bucket según tus necesidades
+
+  # Configuraciones adicionales del bucket de S3 si es necesario
+  tags = {
+    Name = "Mi aplicación"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.static_app_bucket.id
+}
+
