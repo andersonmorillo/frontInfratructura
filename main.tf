1ac43b94bdfe773a06e6cf73bcd6ac3162ec6855
anderson morillo <54423269+andersonmorillo@users.noreply.github.com>
2023-05-22 16:15:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_ecs_task_definition" "my_first_task" {
   container_definitions    = <<DEFINITION
   [
     {
-      "name": "${var.name}:${var.imagebuild}",
+      "name": "frontGrupo0",
       "image": "${var.name}:${var.imagebuild}",
       "essential": true,
       "portMappings": [
