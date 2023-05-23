ec7dbcc5127bba81fbec1ea4a77fb57602614aed
anderson morillo <54423269+andersonmorillo@users.noreply.github.com>
2023-05-23 12:17:49 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  base_url:'http://load-balancer-backendgrupo02-1188414628.us-east-1.elb.amazonaws.com:443/
+  base_url:'http://load-balancer-backendgrupo02-1188414628.us-east-1.elb.amazonaws.com:443/'
 };
